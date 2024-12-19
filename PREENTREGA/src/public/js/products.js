9785670045241232c31f981293d31129c5714cd4
Lucas Verblud <lucasverblud@gmail.com>
2024-12-19 19:28:49 -0300

    EntregaFinal

--- PREENTREGA/src/public/js/products.js
+++ PREENTREGA/src/public/js/products.js
@@ -1,21 +1,40 @@
 const productsList = document.getElementById("products-list");
 const btnRefreshproductsList = document.getElementById("btn-refresh-products-list");
+const btnSortAsc = document.getElementById("btn-sort-asc");
+const btnSortDesc = document.getElementById("btn-sort-desc");
 
-const loadproductsList = async () => {
-    const response = await fetch("/api/products", { method: "GET" });   
+// Función para cargar los productos, con la opción de ordenarlos
+const loadproductsList = async (sortOrder = '') => {
+    const response = await fetch(`/api/products${sortOrder}`, { method: "GET" });   
     const data = await response.json();
     const products = data.payload.docs;
-    productsList.innerText = "";
 
+    // Limpiar la lista antes de agregar los nuevos productos
+    productsList.innerHTML = "";
+
+    // Recorrer los productos y agregar los elementos de lista
     products.forEach((product) => {
-        productsList.innerHTML += `<li>Id: ${product.id} - Nombre: ${product.title} - Descripcion: ${product.description} - Codigo: ${product.code} - Precio: ${product.price} - Status: ${product.status} - Stock: ${product.stock} - Categoria: ${product.category}</li>`;
+        productsList.innerHTML += `<li>Id: ${product.id} - Nombre: ${product.title} - Precio: ${product.price}</li>`;
     });
 };
 
+// Recargar la lista de productos al hacer clic en "Recargar Lista"
 btnRefreshproductsList.addEventListener("click", () => {
     loadproductsList();
     console.log("¡Lista recargada!");
 });
 
-// Se ejecuta para cargar la lista de productes al ingresar o refrescar
+// Ordenar los productos en orden ascendente
+btnSortAsc.addEventListener("click", () => {
+    loadproductsList('?sort=asc');  // '1' es para orden ascendente
+    console.log("Ordenando Ascendente");
+});
+
+// Ordenar los productos en orden descendente
+btnSortDesc.addEventListener("click", () => {
+    loadproductsList('?sort=desc'); // '-1' es para orden descendente
+    console.log("Ordenando Descendente");
+});
+
+// Cargar la lista de productos cuando se ingrese o recargue la página
 loadproductsList();
